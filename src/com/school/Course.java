417228486a86b0bbbcd64e50298ffb3a6bf29e41
sharmaharshitnone <sharmaharshitnone@gmail.com>
2025-07-16 09:31:11 +0530

    Second part of Follow along

---
+++ src/com/school/Course.java
@@ -0,0 +1,16 @@
+package com.school;
+
+public class Course {
+	int courseId;
+	String name;
+
+	public void setDetails(int id, String courseName) {
+		this.courseId = id;
+		this.name = courseName;
+	}
+
+	public void displayDetails() {
+		System.out.println("Course ID: " + this.courseId + ", Name: " + this.name);
+	}
+
+}
--- src/com/school/Main.java
+++ src/com/school/Main.java
@@ -2,7 +2,28 @@ package com.school;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Welcome to the School Attendance System Project!");
-		System.out.println("Session 1: Project Setup and Orientation Complete.");
+		System.out.println("-- School Attendance System --");
+
+		Student[] students = new Student[2];
+		students[0] = new Student();
+		students[0].setDetails(1, "Alice");
+		students[1] = new Student();
+		students[1].setDetails(2, "Bob");
+
+		System.out.println("Registered Students:");
+		for	(Student student : students) {
+			student.displayDetails();
+		}
+
+		Course[] courses = new Course[2];
+  courses[0] = new Course();
+  courses[0].setDetails(101, "Mathematics");
+  courses[1] = new Course();
+  courses[1].setDetails(102, "Science");
+
+		System.out.println("\nAvailable Courses:");
+  for (Course c : courses) {
+    c.displayDetails();
+  }
 	}
 }
